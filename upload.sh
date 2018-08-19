03a4d7dd8bbe5fe08f89d260ff264f4178b9afab
Dreamlu <3147460271@qq.com>
2018-08-19 22:02:34 +0800

    better

--- upload.sh
+++ upload.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 #upload.sh made by lucheng
+#version 1.1
 #server ip and password
-ip=47.94.8.188
-password=lucheng
+ip=
+password=
 uploadDir=root
 #get the newest go build
 go build
 #directory
 curDir=$(basename `pwd`)
-#upload current directory and file
-#$echo $password | scp -r `pwd` root@$ip:/$uploadDir/
-#expect
+cd `pwd`
+cd ..
+#compress the file
+echo "compress tar.gz is running..."
+tar -zcf $curDir.tar.gz $curDir
+echo "compress finshed,try to upload file..."
+#upload the tar.gz file
+#use expect input the password and execute shell
 expect<<EOF
-set timeout 200
-spawn scp -r `pwd` root@$ip:/$uploadDir/
+set timeout 20
+spawn scp -r $curDir.tar.gz root@$ip:/$uploadDir/
 expect "password:" 
 send "$password\r"
 expect eof
 
-#try to run script
+#decompress tar.gz and try to run script
 spawn ssh root@$ip "
-echo 'upload over,try to run progress script deercoder.sh';
-cd /$uploadDir/$curDir;
+echo 'upload over,try to decompress tar.gz';
+cd /$uploadDir/;
+tar -zxf $curDir.tar.gz;
+echo 'decompress finshed,try to run progress script deercoder.sh\n';
+cd $curDir/;
 ./deercoder.sh "
-expect "password:" 
+expect "password:"
 send "$password\r"
+
+#interact      #用exact这个指令是为了把控制权交给用户，代替send "logout\r"    终端不会断开
 expect eof
 EOF
