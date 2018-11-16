2428611420fb3b4d0730a3f48bc531ecc66a3d27
Dreamlu <3147460271@qq.com>
2018-11-16 16:28:42 +0800

    解决windows下编译linux执行权限问题

--- up.sh
+++ up.sh
@@ -8,7 +8,7 @@ uploadDir=root
 #directory
 curDir=$(basename `pwd`)
 #get the newest go build processname
-go build -o $1
+GOOS=linux GOARCH=amd64 go build -o $1
 echo "compress tar.gz is running..."
 tar -zcf $1.tar.gz $1
 echo "$1 compile finished"
