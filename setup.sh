da7b0c3282be2dff1bff3057bfb83c0db53636c4
Dreamlu <3147460271@qq.com>
2018-12-20 10:28:10 +0800

    fix

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-bakdir="/var/spool/cron/crontabs/root"
+if [ $# -lt 1 ]; then 
+    	echo "没有命令行参数,请重新执行"
+	exit
+fi
+bakdir="/etc/crontab"
 if [ ! -f "$bakdir" ]; then
-  touch $bakdir
+  	touch $bakdir
 else
-  echo "定时备份文件已存在"
+  	echo "定时任务文件${bakdir}已存在"
 fi
 
 if grep "bak.sh" $bakdir ; then
 	echo "auto backup added!"
 else
-	echo "34 17 * * * /root/$1/bak.sh $1" >> $bakdir
+	echo -e "#定时备份${1}数据\n1 2 * * * root /root/$1/bak.sh $1" >> $bakdir
+	echo "定时备份==> ${1} 中的数据"
 fi
