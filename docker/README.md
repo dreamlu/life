85f716b7bb9b36edbde16ec00841717df07e95e0
dreamlu <3147460271@qq.com>
2019-12-21 17:14:52 +0800

    整理k8s; 增加go版

--- docker/README.md
+++ docker/README.md
@@ -2,3 +2,6 @@ docker 部署系列
 
 - docker-compose单机  
 - k8s多机器 
+
+> ps: k8s 回滚无缝更新rollout restart方式自动拉取镜像更新,某种因素要求高(目前猜测: 内存)  
+> (线上真实项目崩了,后续通过rollout restart 单个慢慢无缝更新ok)  
--- docker/k8s/pushAll.sh
+++ docker/k8s/pushAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 私有/共有镜像地址
-dockerUrl=registry.cn-hangzhou.aliyuncs.com/deercoder/dreamlu
+dockerUrl=registry.cn-hangzhou.aliyuncs.com/deercoder/dreamlu #registry.gitlab.com/dreamlu/micro-go
 # 批量推向私有仓库
 docker images | grep ${dockerUrl} | awk '{print "docker push "$1":"$2}' | sh
 
