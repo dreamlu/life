4166562242506cd0a5ce8af5c7e83f8b76ea0137
dreamlu <3147460271@qq.com>
2020-04-09 14:10:12 +0800

    修复可执行权限

--- life/shadows/install_centos.sh
+++ life/shadows/install_centos.sh
@@ -3,5 +3,6 @@
 yum update -y
 wget https://github.com/shadowsocks/go-shadowsocks2/releases/download/v0.1.0/shadowsocks2-linux.gz
 gzip -d shadowsocks2-linux.gz
+chmod 755 shadowsocks2-linux
 service firewalld stop
 nohup ./shadowsocks2-linux -s 'ss://AEAD_CHACHA20_POLY1305:dreamlu@:8488' -verbose &
